e7053204634d8b6d3707dff7d9ebff9607151c93
Ferny Dany <fernydany@fernys-mbp.attlocal.net>
2018-05-30 15:34:44 -0700

    Layout Component

--- burger_app/src/Components/Layout/Layout.js
+++ burger_app/src/Components/Layout/Layout.js
@@ -1,8 +1,11 @@
 import React from 'react';
-
+import Aux from '../../Hoc/Aux'
 const layout = (props) => (
+
+    <Aux>
     <div>  Toolbar, Sidedrawer, Backdrop </div>
     <main>
         {props.children}
     </main>
+    </Aux>
 );
