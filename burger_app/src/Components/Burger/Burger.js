cf3a1e49964be2644f116a373bed59a640163157
Ferny Dany <fernydany@fernys-mbp.attlocal.net>
2018-05-31 03:56:56 -0700

    Array flatten method

--- burger_app/src/Components/Burger/Burger.js
+++ burger_app/src/Components/Burger/Burger.js
@@ -3,17 +3,32 @@ import classes from '../Burger/Burger.css';
 import BurgerIngredient from './BurgerIngredients/BurgerIngredients';
 
 const burger = (props) => {
-    const transformedIngredients = Object.keys(props.ingredients);
+    //object.keys outputs an array of keys as strings
+    let transformedIngredients = Object
+        .keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+                return <BurgerIngredient key={igKey + i} type={igKey}/>
+            });
+        }).reduce ((arr,el)=>{
+            //.reduce "flattens the array"
+            return arr.concat(el) },[]);
+        console.log(transformedIngredients);
+
+        if(transformedIngredients.length === 0){
+            transformedIngredients = <p>Please start adding your ingredients:</p>;
+        }
     return (
+
         <div className={classes.Burger}>
 
             <BurgerIngredient type="bread-top"/>
-            <BurgerIngredient type="cheese"/>
-            <BurgerIngredient type="meat"/>
+             {transformedIngredients}
             <BurgerIngredient type="bread-bottom"/>
 
         </div>
     );
-};
+
+}
 
 export default burger;
