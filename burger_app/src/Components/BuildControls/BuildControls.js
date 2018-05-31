40aa9a76528e76f9091ca68c9b451a32e2c2fd99
Ferny Dany <fernydany@fernys-mbp.attlocal.net>
2018-05-31 15:50:16 -0700

    Order Now added

--- burger_app/src/Components/BuildControls/BuildControls.js
+++ burger_app/src/Components/BuildControls/BuildControls.js
@@ -3,23 +3,37 @@ import classes from './BuildControls.css'
 import BuildControl from './BuildControl/BuildControl';
 
 const controls = [
-    {label: 'Salad', type:'salad'},
-    {label: 'Bacon', type:'bacon'},
-    {label: 'Cheese', type:'cheese'},
-    {label: 'Meat', type:'meat'},
+    {
+        label: 'Salad',
+        type: 'salad'
+    }, {
+        label: 'Bacon',
+        type: 'bacon'
+    }, {
+        label: 'Cheese',
+        type: 'cheese'
+    }, {
+        label: 'Meat',
+        type: 'meat'
+    }
 ]
 const buildControls = (props) => (
     <div className={classes.BuildControls}>
-    {controls.map(ctrl => (
-        <BuildControl 
-        key={ctrl.label} 
-        label={ctrl.label}
-        added={() => props.ingredientAdded(ctrl.type)}
-        removed={() => props.ingredientRemoved(ctrl.type)}
-        disabled={props.disabled[ctrl.type]}
-        />
-    ))}
-    </div>
+        <p>Current Price:
+            <strong>
+                {props
+                    .price
+                    .toFixed(2)}
+            </strong>
+        </p>
+        {controls.map(ctrl => (<BuildControl
+            key={ctrl.label}
+            label={ctrl.label}
+            added={() => props.ingredientAdded(ctrl.type)}
+            removed={() => props.ingredientRemoved(ctrl.type)}
+            disabled={props.disabled[ctrl.type]}/>))}
+        <button className={classes.OrderButton} disabled={!props.purchaseable}>ORDER NOW</button>
+    </div> 
 );
 
-export default buildControls; 
+export default buildControls;
--- burger_app/src/Containers/BurgerBuilder/BurgerBuilder.js
+++ burger_app/src/Containers/BurgerBuilder/BurgerBuilder.js
@@ -19,8 +19,21 @@ class BurgerBuilder extends Component  {
             cheese: 0,
             meat: 0
         },
-        totalPrice: 4
+        totalPrice: 4,
+        purchaseable: false
     }
+    updatePurchaseState (ingredients) {
+      
+        const sum = Object.keys(ingredients)
+        .map(igKey => {
+            return ingredients[igKey];
+        })
+        .reduce((sum,el)=> {
+            return sum + el;
+        },0);
+        this.setState({purchaseable: sum >0});
+    }
+
     addIngredientHandler = (type) => {
         const oldCount = this.state.ingredients[type];
         const updatedCount = oldCount + 1;
@@ -32,6 +45,7 @@ class BurgerBuilder extends Component  {
         const oldPrice = this.state.totalPrice;
         const newPrice = oldPrice + priceAddition;
         this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
+        this.updatePurchaseState(updatedIngredients);
     }
     removeIngredientHandler = (type) => {
         const oldCount = this.state.ingredients[type];
@@ -47,6 +61,8 @@ class BurgerBuilder extends Component  {
         const oldPrice = this.state.totalPrice;
         const newPrice = oldPrice - priceDeduction;
         this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
+        this.updatePurchaseState(updatedIngredients);
+        
     }
 
     render () {
@@ -63,6 +79,9 @@ class BurgerBuilder extends Component  {
                  ingredientAdded = {this.addIngredientHandler}
                  ingredientRemoved = {this.removeIngredientHandler}
                  disabled={disabledInfo}
+                 price={this.state.totalPrice}
+                 purchaseable={this.state.purchaseable}
+            
                  />
             </Aux>
         );
