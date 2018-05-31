e97c398ea67a36bd86afc017b16555d26d27b5de
Ferny Dany <fernydany@fernys-mbp.attlocal.net>
2018-05-31 14:13:40 -0700

    Disabled button

--- burger_app/src/Containers/BurgerBuilder/BurgerBuilder.js
+++ burger_app/src/Containers/BurgerBuilder/BurgerBuilder.js
@@ -33,15 +33,37 @@ class BurgerBuilder extends Component  {
         const newPrice = oldPrice + priceAddition;
         this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
     }
-    removeIngredientHandler = () => {
-
+    removeIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type];
+        if ( oldCount <= 0){
+            return;
+        }
+        const updatedCount = oldCount - 1;
+        const updatedIngredients = {
+            ...this.state.ingredients
+        };
+        updatedIngredients[type] = updatedCount;
+        const priceDeduction = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice - priceDeduction;
+        this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
     }
 
     render () {
+        const disabledInfo = {
+            ...this.state.ingredients
+        };
+        for (let key in disabledInfo){
+            disabledInfo[key] = disabledInfo[key] <= 0
+        }
         return (
             <Aux>
                  <Burger ingredients = {this.state.ingredients}/>
-                 <BuildControls ingredientAded = {this.addIngredientHandler}/>
+                 <BuildControls 
+                 ingredientAdded = {this.addIngredientHandler}
+                 ingredientRemoved = {this.removeIngredientHandler}
+                 disabled={disabledInfo}
+                 />
             </Aux>
         );
     }
