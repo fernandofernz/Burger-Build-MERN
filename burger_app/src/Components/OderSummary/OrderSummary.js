8698a78cf5de256c46fac15c5ec89d15281324eb
Ferny Dany <fernydany@fernys-mbp.attlocal.net>
2018-06-22 12:39:38 -0700

    Custom Button added

--- burger_app/src/Components/OderSummary/OrderSummary.js
+++ burger_app/src/Components/OderSummary/OrderSummary.js
@@ -20,6 +20,8 @@ const orderSummary = (props) => {
                 {ingredientSummary}
             </ul>
             <p>Continue to checkout?</p>
+            <button>CANCEL</button>
+            <button>CONTINUE</button>
         </Aux>
     );
 };
