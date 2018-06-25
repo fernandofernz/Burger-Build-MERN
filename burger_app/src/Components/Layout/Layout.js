2f55a31e01457cc98c0558ce2d527fcb2f9d9916
Ferny Dany <fernydany@fernys-mbp.attlocal.net>
2018-06-25 12:33:55 -0700

    Responsive sideDrawer

--- burger_app/src/Components/Layout/Layout.js
+++ burger_app/src/Components/Layout/Layout.js
@@ -2,10 +2,13 @@ import React from 'react';
 import Aux from '../../Hoc/Aux';
 import classes from '../Layout/Layout.css'; 
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
+
 const layout = (props) => (
 
     <Aux>
 <Toolbar/>
+<SideDrawer/>
         <main className={classes.Content}>
         {props.children}
     </main>
