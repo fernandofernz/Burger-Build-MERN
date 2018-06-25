d0b99157179975965484f7b439fb0847b5dd3b1b
Ferny Dany <fernydany@fernys-mbp.attlocal.net>
2018-06-25 11:17:03 -0700

    Reusable nav items added

---
+++ burger_app/src/Components/Navigation/NavigationItems/NavigationItems.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import classes from './NavigationItems.css';
+import NavigationItem from './NavigationItem/NavigationItem';
+
+const navigationItems = (props) => (
+<ul className={classes.NavigationItems}>
+<NavigationItem link="/" active> Burger Builder </NavigationItem>
+<NavigationItem linl="/" > Checkout </NavigationItem>
+
+</ul>
+
+
+);
+
+export default navigationItems;
--- burger_app/src/Components/Navigation/Toolbar/Toolbar.js
+++ burger_app/src/Components/Navigation/Toolbar/Toolbar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import classes from './Toolbar.css';
 import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
 
 const toolbar = (props) => (
     <header className= {classes.Toolbar}>
@@ -9,7 +10,7 @@ const toolbar = (props) => (
     <div>Menu</div>
    <Logo/>
     <nav>
-    ...
+  <NavigationItems/>
     </nav>
     </header>
 );
