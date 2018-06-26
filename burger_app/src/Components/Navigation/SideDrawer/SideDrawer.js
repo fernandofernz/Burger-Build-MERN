5dc9473506dc8cf17efa94a1fed9750259cd2c5e
Ferny Dany <fernydany@fernys-mbp.attlocal.net>
2018-06-26 11:55:44 -0700

    Backdrop with sideDrawer

--- burger_app/src/Components/Navigation/SideDrawer/SideDrawer.js
+++ burger_app/src/Components/Navigation/SideDrawer/SideDrawer.js
@@ -2,11 +2,20 @@ import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './SideDrawer.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../Hoc/Aux';
+
 
 const sideDrawer = (props) => {
 
+    let attachedClasses = [classes.SideDrawer, classes.Close];
+    if (props.open) {
+        attachedClasses = [classes.SideDrawer, classes.Open];
+    }
     return(
-        <div className={classes.SideDrawer}>
+        <Aux>
+        <Backdrop show={props.open} clicked={props.closed}/>
+        <div className={attachedClasses.join(' ')}>
         <div className={classes.Logo}>
             <Logo />
         </div>
@@ -15,6 +24,7 @@ const sideDrawer = (props) => {
                 </nav>
 
         </div>
+        </Aux>
     );
 
 };
