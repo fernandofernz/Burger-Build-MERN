2faabc3b8dc8b2e88587c0a302714b2b9deb3303
Ferny Dany <fernydany@fernys-mbp.attlocal.net>
2018-08-16 11:50:19 -0700

    Axios Post

--- burger_app/src/Containers/BurgerBuilder/BurgerBuilder.js
+++ burger_app/src/Containers/BurgerBuilder/BurgerBuilder.js
@@ -4,7 +4,8 @@ import Burger from '../../Components/Burger/Burger';
 import BuildControls from '../../Components/BuildControls/BuildControls';
 import Modal from '../../Components/UI/Modal/Modal';
 import OrderSummary from '../../Components/OderSummary/OrderSummary';
- 
+import axios from "../../axios-orders";
+
 const INGREDIENT_PRICES = {
     salad: 0.5,
     cheese: 0.4,
@@ -75,7 +76,24 @@ class BurgerBuilder extends Component  {
          this.setState({purchasing: false});
      }
      purchaseContinueHandler = () => {
-         alert ('You may continue!');
+         //alert ('You may continue!');
+         const order = {
+             ingredients: this.state.ingredients,
+             price: this.state.totalPrice,
+             customer: {
+                 name: "Fernz",
+                 address: {
+                     street: "Test Street 123", 
+                     zipcode: 90129,
+                     country: "USA"
+                 },
+                 email: "fernz.email.com"
+             },
+             deliveryMethod: "fastest"
+         }
+         axios.post('/orders.json',order)
+         .then(response => console.log(response))
+         .catch(error => console.log(error));
      }
 
     render () {
